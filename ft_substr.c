ed07a05e4c1f27033a16c07d5885b51f8390d333
zanybeyondart <mailzanyvisuals@gmail.com>
2023-07-19 18:14:36 +0400

    strjoin

--- ft_substr.c
+++ ft_substr.c
@@ -19,47 +19,47 @@ char    *ft_substr(char const *s, unsigned int start, size_t len)
     return (temp);
 }
 
-size_t	ft_mem_l(char const *s, unsigned int start, size_t len)
-{
-	size_t	i;
+// size_t	ft_mem_l(char const *s, unsigned int start, size_t len)
+// {
+// 	size_t	i;
 
-	if (len > strlen(s))
-		i = strlen(s) - start;
-	else
-		i = len + 1;
-	return (i);
-}
+// 	if (len > strlen(s))
+// 		i = strlen(s) - start;
+// 	else
+// 		i = len + 1;
+// 	return (i);
+// }
 
-char	*ft_substr_2(char const *s, unsigned int start, size_t len)
-{
-	char		*str;
-	size_t		i;
+// char	*ft_substr_2(char const *s, unsigned int start, size_t len)
+// {
+// 	char		*str;
+// 	size_t		i;
 
-	if (!s)
-		return (NULL);
-	if (start > strlen(s))
-	{
-		str = (char *)malloc(sizeof(char) * 1);
-		if (!str)
-			return (NULL);
-		str[0] = '\0';
-		return (str);
-	}
-	str = (char *)malloc(sizeof(char) * ft_mem_l(s, start, len));
-	if (!str)
-		return (NULL);
-	i = 0;
-	while (i < len && s[start + i] != '\0')
-	{
-		str[i] = s[start + i];
-		i++;
-	}
-	str[i] = '\0';
-	return (str);
-}
+// 	if (!s)
+// 		return (NULL);
+// 	if (start > strlen(s))
+// 	{
+// 		str = (char *)malloc(sizeof(char) * 1);
+// 		if (!str)
+// 			return (NULL);
+// 		str[0] = '\0';
+// 		return (str);
+// 	}
+// 	str = (char *)malloc(sizeof(char) * ft_mem_l(s, start, len));
+// 	if (!str)
+// 		return (NULL);
+// 	i = 0;
+// 	while (i < len && s[start + i] != '\0')
+// 	{
+// 		str[i] = s[start + i];
+// 		i++;
+// 	}
+// 	str[i] = '\0';
+// 	return (str);
+// }
 
-int main()
-{
-    printf("%s\n", ft_substr_2("Zainali", 0, 9));
-    printf("%s\n", ft_substr("Zainali", 0, 9));
-}
+// int main()
+// {
+//     printf("%s\n", ft_substr_2("Zainali", 0, 9));
+//     printf("%s\n", ft_substr("Zainali", 0, 9));
+// }
