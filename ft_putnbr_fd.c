75e8c82f4c84d54dd901d50d4b5c4a213855330f
zanybeyondart <mailzanyvisuals@gmail.com>
2023-07-29 14:42:14 +0400

    parr2 finih

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -1,4 +1,16 @@
+#include <unistd.h>
+#include <stdio.h>
+
 void ft_putnbr_fd(int n, int fd)
 {
-    
+	int a = 0;
+	if(n >= 10)
+	ft_putnbr_fd(n / 10, fd);
+	a = n % 10 + '0';
+    write(fd, &a, 1);
+}
+
+int main()
+{
+	ft_putnbr_fd(265, 1);
 }
