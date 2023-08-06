ea4bd43016ebaf3ccf67c6a9eae560ae8c68ac28
zanybeyondart <mailzanyvisuals@gmail.com>
2023-08-06 13:44:18 +0400

    part2 finished

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,23 +6,29 @@
 /*   By: zvakil <zvakil@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/29 19:19:51 by zvakil            #+#    #+#             */
-/*   Updated: 2023/07/29 19:19:52 by zvakil           ###   ########.fr       */
+/*   Updated: 2023/08/06 13:43:56 by zvakil           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_putnbr_fd(int n, int fd)
+void	ft_putnbr_fd(int n, int fd)
 {
-	int a = 0;
-	int sign;
+	char	a;
+
+	a = 0;
+	if (n == -2147483648)
+	{
+		write(fd, "-2147483648", 11);
+		return ;
+	}
 	if (n < 0)
 	{
-	write(1, "-", 1);
-	n *= -1;
+		write(fd, "-", 1);
+		n *= -1;
 	}
-	if(n >= 10)
-	ft_putnbr_fd(n / 10, fd);
+	if (n >= 10)
+		ft_putnbr_fd(n / 10, fd);
 	a = n % 10 + '0';
-    write(fd, &a, 1);
+	write(fd, &a, 1);
 }
