31157d855969a7128ed722d8d7fb9e87dcb212cb
zanybeyondart <mailzanyvisuals@gmail.com>
2023-08-06 12:59:20 +0400

    strteri_fixed

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: zvakil <zvakil@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/29 19:19:58 by zvakil            #+#    #+#             */
-/*   Updated: 2023/07/29 19:19:59 by zvakil           ###   ########.fr       */
+/*   Updated: 2023/08/06 12:59:04 by zvakil           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,12 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
 	unsigned int	a;
 
+	if (s == NULL || f == NULL)
+		return ;
 	a = 0;
-    while (s[a] != '\0')
-    {
-        s[a] = f(a, &s[a]);
-        a++;
-    }
+	while (s[a] != '\0')
+	{
+		f(a, &s[a]);
+		a++;
+	}
 }
